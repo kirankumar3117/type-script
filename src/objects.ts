5d2faa4e6f2012ca05baed642b27621f3a8c6c35
kirankumarhexafoldtech <kirankumar.challagiri@hexafoldtech.com>
2023-05-25 23:36:08 +0530

    objects

---
+++ src/objects.ts
@@ -0,0 +1,12 @@
+let employee:{
+    readonly id:number,
+    name:string,
+    retire:(date:Date)=>void
+} = {
+    id:1,
+    name:"",
+    retire:(date:Date)=>{
+        console.log(date)
+    }
+}
+
