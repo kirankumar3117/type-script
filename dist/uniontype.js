a9254fe93d532c43a3e41e622fab611dd442a06d
kirankumarhexafoldtech <kirankumar.challagiri@hexafoldtech.com>
2023-05-26 00:10:37 +0530

    optioanl chaining

--- dist/uniontype.js
+++ dist/uniontype.js
@@ -1,4 +1,5 @@
 "use strict";
+var _a;
 function kgToGrams(weight) {
     if (typeof weight == "number")
         return weight * 2;
@@ -7,6 +8,25 @@ function kgToGrams(weight) {
 }
 console.log(kgToGrams(10));
 console.log(kgToGrams("20kg"));
+let textBox = {
+    drag: () => { },
+    resize: () => { }
+};
+let quantity = 100;
+// Nullable
+function greet(name) {
+    if (name) {
+        console.log(name.toUpperCase());
+    }
+    else {
+        console.log('Hello!');
+    }
+}
+greet(null);
+function getCustomer(id) {
+    return id === 0 ? null : { birthday: new Date };
+}
+let customer = getCustomer(1);
+// Optional property access operator
+console.log((_a = customer === null || customer === void 0 ? void 0 : customer.birthday) === null || _a === void 0 ? void 0 : _a.getFullYear());
 //# sourceMappingURL=uniontype.js.map
-
-
--- src/uniontype.ts
+++ src/uniontype.ts
@@ -45,3 +45,24 @@ function greet(name:string | null | undefined){
 
 greet(null)
 
+// optional chaining
+
+type Customer={
+    birthday?:Date
+}
+
+function getCustomer(id:number):Customer | null | undefined{
+    return id === 0 ? null : {birthday: new Date}
+}
+
+let customer =getCustomer(1);
+
+// Optional property access operator
+
+console.log(customer?.birthday?.getFullYear())
+
+// Optional element operator (usefull when we are dealing with array)
+
+
+
+
