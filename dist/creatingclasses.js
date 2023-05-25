7c85ba232a149512d45e207f4adf15bd6e08afc1
kirankumarhexafoldtech <kirankumar.challagiri@hexafoldtech.com>
2023-05-26 01:18:59 +0530

    creating a object

--- dist/creatingclasses.js
+++ dist/creatingclasses.js
@@ -11,3 +11,9 @@ class Account {
         this.balance += amount;
     }
 }
+let account = new Account(1, "Kiran", 0);
+account.deposit(100);
+console.log(account.balance);
+console.log(account);
+console.log(typeof account);
+console.log(account instanceof Account);
--- src/creatingclasses.ts
+++ src/creatingclasses.ts
@@ -15,3 +15,12 @@ class Account{
     }
 
 }
+
+let account=new Account(1, "Kiran", 0);
+
+account.deposit(100);
+
+console.log(account.balance);
+console.log(account);
+console.log(typeof account)
+console.log(account instanceof Account)
