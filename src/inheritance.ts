5e24505d9a91533a78506a8d3243e05b8a883120
kirankumarhexafoldtech <kirankumar.challagiri@hexafoldtech.com>
2023-05-26 13:56:06 +0530

    abstract classe and methods

--- src/inheritance.ts
+++ src/inheritance.ts
@@ -57,3 +57,22 @@ function printNames(people:Person[]){
         console.log(person.fullName)
 }
 
+// Abstract classes
+
+abstract class Shape{
+    constructor(public color:string){}
+
+    abstract render():void;
+}
+
+class Circle extends Shape{
+    constructor(public radius:number,color: string){
+        super(color)
+    }
+
+    override render(): void {
+        console.log("Rendering a Circle !")
+    }
+
+} 
+
